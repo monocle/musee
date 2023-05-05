a6b4fd711f89bbb3023f3fa64273ad3dfae0c813
monocle <monocle.github@gmail.com>
2023-05-05 16:41:48 -0700

    Update MSW to handle pagination for paintings

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,15 +1,15 @@
 import { rest } from "msw";
-import { Painting } from "../types";
-
-const paintings: Painting[] = [];
+import paintingsResponse from "./paintings.json";
 
 export const handlers = [
-  rest.get("api/paintings", (_, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        paintings,
-      })
-    );
+  rest.get("api/paintings", async (req, res, ctx) => {
+    const maxRecords = paintingsResponse.records.length;
+    const offset = Number(req.url.searchParams.get("offset") ?? "0");
+    const limit = Number(req.url.searchParams.get("limit") ?? "20");
+    const first = offset < 0 ? 0 : offset;
+    const last = Math.min(first + limit, maxRecords);
+    const records = paintingsResponse.records.slice(first, last);
+
+    return res(ctx.status(200), ctx.json({ records, count: maxRecords }));
   }),
 ];
