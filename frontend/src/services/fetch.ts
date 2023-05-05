b3470701a193657d9c10f6f8c8debef89ce54a00
monocle <monocle.github@gmail.com>
2023-05-05 16:42:52 -0700

    Add query params to fetch.get()

--- frontend/src/services/fetch.ts
+++ frontend/src/services/fetch.ts
@@ -2,12 +2,11 @@ import axios from "axios";
 
 const API_PREFIX = "/api/";
 
-export function get(path: string) {
-  return () =>
-    axios
-      .get(API_PREFIX + path)
-      .then((response) => response.data)
-      .catch((e) => {
-        throw e.response.data;
-      });
+export async function get(path: string, params: unknown) {
+  return axios
+    .get(API_PREFIX + path, { params })
+    .then((response) => response.data)
+    .catch((e) => {
+      throw e.response.data;
+    });
 }
