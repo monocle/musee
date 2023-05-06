b551d115bd9a7d05ba1d38c5aae8a0371fe9738f
monocle <monocle.github@gmail.com>
2023-05-06 08:35:54 -0700

    Update Landing.tsx

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,3 +1,40 @@
 export default function Landing() {
-  return <>Welcome</>;
+  return (
+    <>
+      <div
+        className="hero h-[80vh]"
+        style={{
+          backgroundImage: `url("https://ids.lib.harvard.edu/ids/view/18772450")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-40"></div>
+        <div className="hero-content text-center">
+          <div className="max-w-md text-gray-100">
+            <h1 className="mb-5 text-5xl font-bold">Artwork to Inspire</h1>
+            <p className="mx-auto mb-5 w-72">
+              Explore museum paitings and collect the ones that inspire you.
+            </p>
+            <button className="btn-accent btn">Get Started</button>
+          </div>
+        </div>
+      </div>
+
+      <aside className="flex gap-8 px-2 py-2">
+        <p className="font-semibold">
+          Peach Blossom Spring (after Shao Zhenxian)
+        </p>
+        <p>Ikeno Taiga 池大雅, Japanese (Kyoto 1723 - 1776)</p>
+        <cite>
+          <a
+            className="link"
+            href="https://harvardartmuseums.org/collections/object/201168"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Harvard Art Museum
+          </a>
+        </cite>
+      </aside>
+    </>
+  );
 }
