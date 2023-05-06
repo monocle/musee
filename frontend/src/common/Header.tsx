9dae5b0a6ef020d636f3dd62059d5dd6badd44fd
monocle <monocle.github@gmail.com>
2023-05-05 17:19:55 -0700

    Add stub Landing page

--- frontend/src/common/Header.tsx
+++ frontend/src/common/Header.tsx
@@ -1,7 +1,19 @@
+import { Link } from "@tanstack/react-router";
+
 export default function Header() {
   const isDemo = import.meta.env.MODE === "demo";
 
   return (
-    <header>{isDemo && <div className="text-sm">(Demo Mode)</div>}</header>
+    <header className="navbar flex items-center justify-between bg-base-100 px-4 py-2">
+      <Link to="/" className="text-xl font-semibold hover:text-blue-500">
+        Musee
+      </Link>
+
+      {isDemo && <div className="text-sm">(Demo Mode)</div>}
+
+      <Link to="/paintings" className="font-semibold hover:text-blue-500">
+        Paintings
+      </Link>
+    </header>
   );
 }
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,18 +1,25 @@
 import { Route, Router, RootRoute } from "@tanstack/react-router";
 import App from "./App";
+import Landing from "./pages/Landing";
 import Paintings from "./paintings/Paintings";
 
 const rootRoute = new RootRoute({
   component: App,
 });
 
-const paintingsRoute = new Route({
+const landingRoute = new Route({
   getParentRoute: () => rootRoute,
   path: "/",
+  component: Landing,
+});
+
+const paintingsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/paintings",
   component: Paintings,
 });
 
-const routeTree = rootRoute.addChildren([paintingsRoute]);
+const routeTree = rootRoute.addChildren([landingRoute, paintingsRoute]);
 
 const router = new Router({ routeTree });
 
