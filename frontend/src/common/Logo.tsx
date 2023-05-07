a2e186e06f372f8950f2aa525c7611b166c60fdd
monocle <monocle.github@gmail.com>
2023-05-07 09:08:52 -0700

    Add demo badge to Logo

--- frontend/src/common/Logo.tsx
+++ frontend/src/common/Logo.tsx
@@ -2,17 +2,23 @@ import { Link } from "@tanstack/react-router";
 
 interface Props {
   className?: string;
+  showDemo?: boolean;
 }
 
-export default function Logo({ className = "" }: Props) {
+export default function Logo({ className = "", showDemo = true }: Props) {
   const isDemo = import.meta.env.MODE === "demo";
 
   return (
-    <Link
-      to="/"
-      className={`text-xl font-semibold hover:text-blue-500 ${className}`}
-    >
-      Musee {isDemo && <span className="pl-4 text-xs">(Demo Mode)</span>}
-    </Link>
+    <div className="flex items-center">
+      <Link
+        to="/"
+        className={`text-xl font-semibold text-info hover:text-blue-500 ${className}`}
+      >
+        Musee
+      </Link>
+      {isDemo && showDemo && (
+        <span className="badge ml-4 text-xs italic">Demo</span>
+      )}
+    </div>
   );
 }
