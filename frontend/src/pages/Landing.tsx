da0b8054f0af37c85d21b7a42e88246ee4409a9d
monocle <monocle.github@gmail.com>
2023-05-07 09:16:32 -0700

    Update Landing
    * Wait until hero image is loaded before displaying main content then fade in
    * Show spinner while waiting
    * Add full painting record
    * Refactor out <BriefPaintingInfo>

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,47 +1,85 @@
+import { useState } from "react";
 import { Link } from "@tanstack/react-router";
+import type { Painting } from "../types";
 import Header from "../common/Header";
+import Spinner from "../svgs/Spinner";
+import BriefPaintingInfo from "../paintings/BriefPaintingInfo";
+
+const heroPainting: Painting = {
+  images: [],
+  imagepermissionlevel: 0,
+  dated: "Mid Edo period, circa 1757",
+  dateend: 1757,
+  id: 201168,
+  medium:
+    "Album leaf or handscroll section(?) mounted as a hanging scroll; ink and light colors on paper; with artist's seal reading Sangaku Dōja",
+  title: "Peach Blossom Spring (after Shao Zhenxian)",
+  primaryimageurl: "https://nrs.harvard.edu/urn-3:HUAM:DDC100686_dynmc",
+  people: [
+    {
+      role: "Artist",
+      birthplace: "Kyoto",
+      gender: "male",
+      displaydate: "1723 - 1776",
+      prefix: null,
+      culture: "Japanese",
+      displayname: "Ikeno Taiga 池大雅",
+      alphasort: "Ikeno, Taiga",
+      name: "Ikeno Taiga 池大雅",
+      personid: 28943,
+      deathplace: null,
+      displayorder: 1,
+    },
+  ],
+  colors: [],
+  url: "https://www.harvardartmuseums.org/collections/object/201168",
+  dimensions:
+    "painting proper: H. 30.7 x W. 32.6 cm (12 1/16 x 12 13/16 in.)\r\nmounting, including cord and roller ends: H. 132.7 x W. 54.6 cm (52 1/4 x 21 1/2 in.)",
+};
 
 export default function Landing() {
+  const [isImgLoaded, setIsImgLoaded] = useState(false);
+
   return (
     <>
       <Header />
+      {!isImgLoaded && (
+        <div className="hero h-[80vh]">
+          <Spinner />
+        </div>
+      )}
       <div
-        className="hero h-[80vh]"
-        style={{
-          backgroundImage: `url("https://ids.lib.harvard.edu/ids/view/18772450")`,
-        }}
+        className={
+          isImgLoaded
+            ? "opacity-100 transition-opacity duration-1000"
+            : "opacity-0"
+        }
       >
-        <div className="hero-overlay bg-opacity-40"></div>
-        <div className="hero-content text-center">
-          <div className="max-w-md text-gray-100">
-            <h1 className="mb-5 text-5xl font-bold">Artwork to Inspire</h1>
-            <p className="mx-auto mb-5 w-72">
-              Explore museum paitings and collect the ones that inspire you.
-            </p>
+        <div className="hero relative h-[80vh]">
+          <img
+            src={heroPainting.primaryimageurl}
+            alt={heroPainting.title}
+            className="absolute left-0 top-0 h-full w-full object-cover opacity-60"
+            onLoad={() => setIsImgLoaded(true)}
+          />
+          <div className="hero-content text-center">
+            <div className="max-w-md text-gray-100">
+              <h1 className="mb-5 text-5xl font-bold">Artwork to Inspire</h1>
+              <p className="mx-auto mb-5 w-72">
+                Explore museum paitings and collect the ones that inspire you.
+              </p>
 
-            <Link to="/painting">
-              <button className="btn-accent btn">Get Started</button>
-            </Link>
+              <Link to="/painting">
+                <button className="btn-accent btn">Get Started</button>
+              </Link>
+            </div>
           </div>
         </div>
+        <BriefPaintingInfo
+          painting={heroPainting}
+          source="Harvard Art Museum"
+        />
       </div>
-
-      <aside className="flex gap-8 px-2 py-2">
-        <p className="font-semibold">
-          Peach Blossom Spring (after Shao Zhenxian)
-        </p>
-        <p>Ikeno Taiga 池大雅, Japanese (Kyoto 1723 - 1776)</p>
-        <cite>
-          <a
-            className="link"
-            href="https://harvardartmuseums.org/collections/object/201168"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Harvard Art Museum
-          </a>
-        </cite>
-      </aside>
     </>
   );
 }
