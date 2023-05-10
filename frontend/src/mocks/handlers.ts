5292cc9306fb85d56685efd546de65bf4f9c6435
monocle <monocle.github@gmail.com>
2023-05-09 19:43:57 -0700

    Have MSW fetch data from files

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,8 +1,9 @@
 import { rest } from "msw";
-import paintingsResponse from "./ham_paintings_1.json";
 
 export const handlers = [
   rest.get("/api/paintings", async (req, res, ctx) => {
+    const fetchRes = await fetch("/ham_paintings_1.json");
+    const paintingsResponse = await fetchRes.json();
     const maxRecords = paintingsResponse.records.length;
     const offset = Number(req.url.searchParams.get("offset") ?? "0");
     const limit = Number(req.url.searchParams.get("limit") ?? "20");
