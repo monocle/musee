008a9aa1338fe2dd3dc3c9b844bcdbb4435d9ab9
monocle <monocle.github@gmail.com>
2023-05-09 19:43:11 -0700

    Add useLocalStorage hook

---
+++ frontend/src/services/useLocalStorage.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+
+type SetValue<T> = Dispatch<SetStateAction<T>>;
+type UseLocalStorageReturnType<T> = [T, SetValue<T>];
+
+function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): UseLocalStorageReturnType<T> {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error("Error reading from localStorage:", error);
+      return initialValue;
+    }
+  });
+
+  const setValue: SetValue<T> = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.error("Error setting value in localStorage:", error);
+    }
+  };
+
+  useEffect(() => {
+    const handleStorageChange = (e: StorageEvent) => {
+      if (e.key === key) {
+        setStoredValue(JSON.parse(e.newValue as string));
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, [key]);
+
+  return [storedValue, setValue];
+}
+
+export default useLocalStorage;
