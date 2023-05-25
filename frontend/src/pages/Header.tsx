710ab88ee726b72f5a27849c125e9f3e11d45742
monocle <monocle.github@gmail.com>
2023-05-25 12:05:19 -0700

    Hide nav when click happens outside of it

--- frontend/src/pages/Header.tsx
+++ frontend/src/pages/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import CollectionLink from "./CollectionLink";
@@ -7,12 +7,38 @@ import ThemeToggler from "./ThemeToggle";
 
 export default function Header() {
   const [showNav, setShowNav] = useState(false);
+  const navRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const listener = showNav
+      ? document.addEventListener
+      : document.removeEventListener;
+
+    const handleClickOutside = (event: MouseEvent) => {
+      const isOutsideClick =
+        navRef.current &&
+        event.target instanceof Element &&
+        !navRef.current.contains(event.target);
+
+      if (isOutsideClick) setShowNav(false);
+    };
+
+    listener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showNav]);
 
   return (
     <header className="sticky top-0 z-30 flex items-center justify-between bg-base-200 px-4">
       <Logo />
 
-      <nav className="flex items-center gap-1 md:gap-6" aria-label="Site">
+      <nav
+        className="flex items-center gap-1 md:gap-6"
+        ref={navRef}
+        aria-label="Site"
+      >
         <button
           className="btn-ghost btn"
           onClick={() => setShowNav(!showNav)}
@@ -21,7 +47,7 @@ export default function Header() {
           <FontAwesomeIcon icon={faBars} className="h-5" />
         </button>
         <div
-          className={`absolute right-0 top-12 z-50 flex w-44 transform flex-col items-center gap-2 rounded bg-base-300 px-2 py-4 transition-all duration-200 ease-out ${
+          className={`absolute right-0 top-12 z-50 flex w-44 transform flex-col items-center gap-2 rounded bg-base-300 px-2 py-4 transition-all duration-300 ease-out ${
             showNav ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"
           }`}
         >
