600e7c3876c0bdacf24227bc4ca5968d91183b58
monocle <monocle.github@gmail.com>
2023-05-26 13:57:56 -0700

    Add Landing page tests

--- frontend/src/common/CenterScreenSpinner.tsx
+++ frontend/src/common/CenterScreenSpinner.tsx
@@ -2,7 +2,10 @@ import Spinner from "./Spinner";
 
 export default function CenterScreenSpinner() {
   return (
-    <div className="fixed top-0 flex h-screen w-screen items-center justify-center">
+    <div
+      className="fixed top-0 flex h-screen w-screen items-center justify-center"
+      data-testid="large spinner"
+    >
       <Spinner />
     </div>
   );
---
+++ frontend/src/pages/Landing.test.tsx
@@ -0,0 +1,18 @@
+import {
+  createTestRouter,
+  renderComponent,
+  screen,
+  waitFor,
+} from "../test/testHelpers";
+import Landing from "./Landing";
+
+const router = createTestRouter(Landing);
+
+test("renders Landing component", async () => {
+  renderComponent(router);
+
+  await waitFor(() => {
+    expect(screen.getByTestId("large spinner")).toBeInTheDocument();
+    expect(screen.queryByText(/artwork to inspire/i)).toBeInTheDocument();
+  });
+});
