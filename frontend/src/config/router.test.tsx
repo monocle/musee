856e52b35233e6810e526bb7934fc8f49a320df5
monocle <monocle.github@gmail.com>
2023-05-29 15:53:18 -0700

    Refactor router tests

--- frontend/src/config/router.test.tsx
+++ frontend/src/config/router.test.tsx
@@ -1,53 +1,60 @@
-import { renderComponent, screen, userEvent, waitFor } from "../testing";
+import {
+  renderComponent,
+  screen,
+  userEvent,
+  expectInDocument,
+  expectNotInDocument,
+} from "../testing";
 import { serveData } from "../testing";
 import * as data from "../testing/data/aic_collection_response_2.json";
 import * as favorites from "../testing/data/favorites_response.json";
 import router from "./router";
 
+const record0 = data.records[0];
+const record1 = data.records[1];
+const favorite0 = favorites.records[0];
+
+async function expectLandingPage() {
+  await expectNotInDocument(record0.title);
+  await expectNotInDocument(record1.title);
+  await expectNotInDocument(favorite0.title);
+}
+
+async function expectCollectionPage() {
+  await expectNotInDocument(/artwork to inspire/i);
+  await expectInDocument(record0.title);
+  await expectInDocument(record1.title);
+  await expectNotInDocument(favorite0.title);
+}
+
 // Using one test until proper testing with TanStack Router is figured out.
 test("all routing links", async () => {
   const user = userEvent.setup();
-  const record0 = data.records[0];
-  const record1 = data.records[1];
-  const favorite0 = favorites.records[0];
 
   serveData({ path: "collections/aic", data });
   serveData({ path: "collections/favorites", data: favorites });
   renderComponent(router);
 
   // Initial landing page
-  await waitFor(() => {
-    expect(screen.queryByText(/artwork to inspire/i)).toBeInTheDocument();
-  });
+  await expectLandingPage();
 
   // Hero button link
   await user.click(screen.getByRole("link", { name: /let's go/i }));
-  await waitFor(() => {
-    expect(screen.queryByText(/artwork to inspire/i)).not.toBeInTheDocument();
-    expect(screen.queryByText(record0.title)).toBeInTheDocument();
-    expect(screen.queryByText(record1.title)).toBeInTheDocument();
-    expect(screen.queryByText(favorite0.title)).not.toBeInTheDocument();
-  });
+  await expectCollectionPage();
 
   // Logo link back to home
   await user.click(screen.getByLabelText(/navigate to home/i));
-  await waitFor(() => {
-    expect(screen.queryByText(/artwork to inspire/i)).toBeInTheDocument();
-  });
+  await expectLandingPage();
 
   // Favorites menu link
   await user.click(screen.getByRole("link", { name: /favorites/i }));
-  await waitFor(() => {
-    expect(screen.queryByText(record0.title)).not.toBeInTheDocument();
-    expect(screen.queryByText(record1.title)).not.toBeInTheDocument();
-    expect(screen.queryByText(favorite0.title)).toBeInTheDocument();
-  });
+
+  await expectNotInDocument(/artwork to inspire/i);
+  await expectNotInDocument(record0.title);
+  await expectNotInDocument(record1.title);
+  await expectInDocument(favorite0.title);
 
   // All menu link
   await user.click(screen.getByRole("link", { name: /all/i }));
-  await waitFor(() => {
-    expect(screen.queryByText(record0.title)).toBeInTheDocument();
-    expect(screen.queryByText(record1.title)).toBeInTheDocument();
-    expect(screen.queryByText(favorite0.title)).not.toBeInTheDocument();
-  });
+  await expectCollectionPage();
 });
